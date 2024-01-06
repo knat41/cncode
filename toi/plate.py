956bace47ae934a0caa2efb5c3dbd41604cf38be
knat41 <75963145+knat41@users.noreply.github.com>
2024-01-06 16:45:17 +0700

    Update plate.py

--- toi/plate.py
+++ toi/plate.py
@@ -1,7 +1,8 @@
 '''toi1_plate'''
 nc, ns = [int(x) for x in input().split()]
 lines = []
-cline = {}
+dline = {}
+cline = []
 registration = {}
 while ns > 0:
     c, s = [int(x) for x in input().split()]
@@ -12,9 +13,15 @@ cmd = ''
 while cmd != 'X':    
     match input().split():
         case ['E', s_id]:
-            pass
+            if len(lines) == 0:
+                lines.append(s_id)
+                if s_id not in dline:
+                    dline[s_id] = 1
+                else:
+                    dline[s_id] += 1
         case ['D']:
-            pass
+            if len(lines) == 0:
+                print("empty")
         case ['X']:
             print(0)
             break
